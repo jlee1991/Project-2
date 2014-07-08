fe60b9acd00781ba213b256cd90e839df3b827a5
Jim Lee <jlee91@bu.edu>
2014-07-08 17:36:28 -0400

    made edits to README and index file

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);*/ # Display errors on page (instead of a log file)
 <html>
 <head>
 
-    <title>XKCD Random Number Generator</title>
+    <title>XKCD Random Password Generator</title>
     <meta charset="utf-8">
     <link rel='stylesheet' href='' type='text/css'>
     <?php require 'logic.php'; ?>
@@ -15,7 +15,8 @@ ini_set('display_errors', 1);*/ # Display errors on page (instead of a log file)
 </head>
 
 <body>
-  <p><b>XKCD Random Number Generator</b></p> <br>
+  <p><b>XKCD Random Password Generator</b></p> <br>
+  <p>Welcome to the XKCD Random Password Generator. To give a brief introduction to this website, you merely need to enter in number that corresponds with the number of words needed in your password and check the number box or symbol box to include a number or symbol in your password!</p>
   <form action='index.php' method='GET'>
     # of Words (Max 9): <input type="text" value="" name="subject" maxlength="1"><br>
     <input type="checkbox" value="Yes" name="number">Add Number<br>
