b9a9cba4f578f39663f96bad5e51a26b7cd6f170
Jim Lee <jlee91@bu.edu>
2014-07-08 17:24:39 -0400

    final product

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+/*error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1);*/ # Display errors on page (instead of a log file)
 ?>
 
 <!DOCTYPE html>
@@ -15,23 +15,13 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 </head>
 
 <body>
-
+  <p><b>XKCD Random Number Generator</b></p> <br>
   <form action='index.php' method='GET'>
-    # of Words (Max 9): <input type="text" name="" maxlength="1"><br>
+    # of Words (Max 9): <input type="text" value="" name="subject" maxlength="1"><br>
+    <input type="checkbox" value="Yes" name="number">Add Number<br>
+    <input type="checkbox" value="Yes" name="symbol">Add Symbol<br>
     <input type="submit" value="Submit">
   </form>
-
-	<?php //foreach($lines as $iteration => &$result): ?>
-		<?php //echo $result[$word] ?>
-	<?php //endforeach; ?>
-
-  <?php //foreach($name as $name => &$result): ?>
-      <?php //echo $name; ?>
-      <?php //for($i=0; $i<$iteration; $i++) ?>
-      <?php //{ ?>
-        <?//php echo $password[$i] ?>
-      <?php //} ?>
-  <?php //endforeach; ?>
 
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,23 +1,30 @@
 <?php
+//Number of words from index.php
+/*if ($_GET['subject']=0)
+  {$iteration=0;}
+else
+  {;}*/
+$iteration=$_GET['subject'];
 //Array from text file
 $lines = file('text.txt');
-for($i=0; $i<5; $i++) {
+for($i=0; $i<$iteration; $i++) {
  foreach($lines as $line) {
    $word = rand(0,5000);
  }
  $line=$lines[$word];
- //echo $line. "<br />";
+ echo $line."-";
+ //echo $line."-";
 }
-?>
 
-<?php
+if ($_GET['symbol']=='Yes'){
+  $symbol="&";
+  echo $symbol;
+ }
+else{};
 
-  /*$text = file('text.txt'); //Array from text file
-
-  for($i=0; $i<$iteration; $i++) {
-    foreach() {
-      $word = rand(0,5000);
-      $password['$iteration'] = $text['$word'];
-    }
-  } */
+if ($_GET['number']=='Yes'){
+  $number = rand(0,9);
+  echo $number;
+ }
+else{};
 ?>
